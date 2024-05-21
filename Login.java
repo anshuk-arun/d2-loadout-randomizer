ee69b5cbb330c6492ceb8b04e309c633db10d3f1
anshuk-arun <64498261+anshuk-arun@users.noreply.github.com>
2024-05-21 13:01:08 -0400

    Create Login.java and Upload D2 Pic
    Created Login.java
    this will serve as the intro landing page of the application. Users will (eventually) be prompted to login with their Bungie credentials.
    
    Added a D2 pic to test out committing images.

---
+++ Login.java
@@ -0,0 +1,15 @@
+/* Login.java
+ * First Page Landing
+ * 
+ *  Login Button to redirect to Bungie
+ * 
+ */
+
+ public class Login{
+
+    public static void main(String[] args){
+
+        System.out.println("This is the first landing page of my application.");
+    }
+
+ }
