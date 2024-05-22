c655004bad1bdb32d9a498b242fdbb91c7f6c0ca
anshuk-arun <64498261+anshuk-arun@users.noreply.github.com>
2024-05-22 17:16:39 -0400

    Added Panel to Frame
    Added a Panel to the Frame.
    Content is added to the Panel, not the Frame directly.
    
    Set up the grounds for the Background Image.

--- TopLevelWindow.java
+++ TopLevelWindow.java
@@ -10,18 +10,27 @@ public class TopLevelWindow {
         JFrame frame = new JFrame("Login Page");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Create a panel inside the window
+        // Add the Panel to the window
+        JPanel panel = new JPanel(new BorderLayout());
+        frame.add(panel);
+
         JLabel textLabel = new JLabel("I'm a label in the window and this is the first Login page", SwingConstants.CENTER);
         textLabel.setPreferredSize(new Dimension(300, 100));
-        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+        panel.add(textLabel, BorderLayout.CENTER);
+        //frame.getContentPane().add(textLabel, BorderLayout.CENTER);
 
         JButton loginButton = new JButton("Login to Bungie");
         loginButton.setPreferredSize(new Dimension(200, 100));
-        frame.getContentPane().add(loginButton, BorderLayout.SOUTH);
+        panel.add(loginButton, BorderLayout.SOUTH);
+        //frame.getContentPane().add(loginButton, BorderLayout.SOUTH);
+        
 
-
-        JLabel imageHolder = new JLabel();
-        //imageHolder.setIcon(makeImageIcon("MomentsOfTriumph2020.jpg"));
-        frame.getContentPane().add(imageHolder);
+        // Background Image that currently takes over the window.
+        // JLabel imageHolder = new JLabel();
+        // ImageIcon d2Icon = new ImageIcon("images/D2_Logo.png");
+        // imageHolder.setIcon(d2Icon);
+        // frame.getContentPane().add(imageHolder);
 
         // Display the Window
         frame.setLocationRelativeTo(null);
